82df7fe1dd572d40ec1db2baab404e6ec647d389
Jonathan Beaty <jonathanbeaty15@gmail.com>
2018-11-04 07:35:29 -0600

    tracking progress

--- models.js
+++ models.js
@@ -2,3 +2,77 @@
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+
+// HOME_PAGE 
+
+const frontPageSliderSchema = mongoose.Schema({
+    img: {
+        data: Buffer,
+        contentType: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const localFavoritesImgSchema = mongoose.Schema({
+    img: {
+        data: Buffer,
+        contentType: String,
+        required: true
+    },
+    bikiniName: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+const upcomingEventsSchema = mongoose.Schema({
+    img: {
+        data: Buffer,
+        contentType: String,
+        required: true
+    },
+    eventName: {
+        type: String,
+        required: true
+    },
+    eventDate: {
+        type: Date,
+        required: true
+    },
+    eventLocation: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+
+
+frontPageSliderSchema.methods.serialize = function () {
+    return {
+        id: this._id,
+        img: this.img,
+        description: this.description,
+        created: this.created
+    };
+};
